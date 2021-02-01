cfd7c4c9f1cfc780d678938b21d54320cc595f87
Jaysmitio101 <73700725+Jaysmitio101@users.noreply.github.com>
2021-02-01 19:31:45 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import com.jaysmito.jsubs.JSubsConstants;
+import com.jaysmito.jsubs.SubsFrame;
+import com.jaysmito.jsubs.Toast;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * Returns nothing.
+     *
+     * <p>
+     * This method always returns immediately.
+     * Demo method.
+     *
+     * @see         Toast
+     */
+    public static void main(String[] args) throws Exception{
+        Toast.showToast("Hello World!");
+    }
+}
+
